17ab10ec4218c97d846880c769d5afdb6a3e75de
GUILHERME ROSA LOPES <guilherme.rlopes6@senacsp.edu.br>
2025-02-24 21:32:52 -0300

    att sw

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // Se o navegador de internet suportar este recurso
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('./jokenpoFLEX/sw.js')
+        .register('/sw.js')
         .then(() => {
             console.log("Service Worker registrado!")
         })
